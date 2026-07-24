cbdb18114a1ac9f8a5782a698ececaead8d711b6
hong jing song <1053440045@qq.com>
2026-07-24 10:26:56 +0800

    修改版本号

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -1206,7 +1206,7 @@ int cmd_version(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    ESP_LOGI(TAG, "firmware version: V1.01");
+    ESP_LOGI(TAG, "firmware version: V1.02");
     ESP_LOGI(TAG, "FLRC TX power: %d dBm", APP_FLRC_TX_POWER_DBM);
     return 0;
 }
@@ -1511,7 +1511,7 @@ extern "C" void app_main(void)
              APP_LCD_H_RES, APP_LCD_V_RES);
     ESP_LOGI(TAG, "K5: switch camera/radio mode. K6/PTT: FLRC voice in radio mode. K4=vol+, K3=vol-");
 
-    ESP_LOGI(TAG, "firmware version: V1.01");
+    ESP_LOGI(TAG, "firmware version: V1.02");
     ESP_LOGI(TAG, "FLRC TX power: %d dBm", APP_FLRC_TX_POWER_DBM);
 
     // Console: type "version" to reprint version + TX power at any time.
